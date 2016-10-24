ec21fd202c9c757a4cd580d12bcdaeefb248240d
nhodge253 <nhodge253@gmail.com>
2016-10-24 00:55:07 -0400

    finished manipulating DOM tasks

--- js/main.js
+++ js/main.js
@@ -2,36 +2,7 @@
 // WATS1020 Dom Manipulation
 // Custom script goes here.
 //////////////////////////////////////////////////
-
-$( document ).ready(function() {
-    var userInfo = {
-        firstName: 'Jane',
-        lastName: 'Doe'
-    };
-    var voteCounts = {
-        great: 0,
-        greatest: 0,
-        total: 0
-    };
-  $("#login-form .btn-sm").click(function(){
- $("#login-form").hide();//This says that when you click the login button, it hides the form.
-    $(".user-info").text("Welcome, " + userInfo.firstName +" "+ userInfo.lastName+"!");//This continues and says to replace the form with the text, but doesn't show the text yet.
-    $(".user-info").show();// This completes the function by showing the text.
-});
-  //lines 22-24 picks and shows the details by using parent and child element.
-  $(".view-details").click(function(event){ //selects the view details click throughouth the page and then sets a function that happens when the view details buttons are clicked.
-  var targetElement = event.target;
-  var childDetails = $(targetElement).parent().parent().children(".details");// created variable childDetails to simplify next code.  This variable defines each individual details elements with the parent parent children command.
-    if ( $(childDetails).is(":visible") === true){
-       $(childDetails).hide();
-  $(targetElement).text("View details");//Commands to hide the details if they are already shown.
-    }
-    else { $(childDetails).show();
-  $(targetElement).text("Hide details");//Commands to show the details if they are hidden.
-    }
- 
-});
-    // Place all your Javascript code inside this "document ready" function so
+// Place all your Javascript code inside this "document ready" function so
     // it does not run until the DOM is ready for Javascript manipulation.
 
     // TODO: Create a function to listen for clicks on the "login" button.
@@ -61,5 +32,48 @@ $( document ).ready(function() {
     //      3. Increment the counter for whichever vote talley is affected.
     //      4. Determine the respective percentages (out of 100) for each progress bar.
     //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+
+$( document ).ready(function() {
+    var userInfo = {
+        firstName: 'Jane',
+        lastName: 'Doe'
+    };
+    var voteCounts = {
+        great: 0,
+        greatest: 0,
+        total: 0
+    };
+  $("#login-form .btn-sm").click(function(){
+ $("#login-form").hide();//This says that when you click the login button, it hides the form.
+    $(".user-info").text("Welcome, " + userInfo.firstName +" "+ userInfo.lastName+"!");//This continues and says to replace the form with the text, but doesn't show the text yet.
+    $(".user-info").show();// This completes the function by showing the text.
+});
+  //lines 22-24 picks and shows the details by using parent and child element.
+  $(".view-details").click(function(event){ //selects the view details click throughouth the page and then sets a function that happens when the view details buttons are clicked.
+  var targetElement = event.target;
+  var childDetails = $(targetElement).parent().parent().children(".details");// created variable childDetails to simplify next code.  This variable defines each individual details elements with the parent parent children command.
+    if ( $(childDetails).is(":visible") === true){
+       $(childDetails).hide();
+  $(targetElement).text("View details");//Commands to hide the details if they are already shown.
+    }
+    else { $(childDetails).show();
+  $(targetElement).text("Hide details");//Commands to show the details if they are hidden.
+    }
+ 
+});
+   $(".vote").click(function (event) {
+       var targetElement = event.target; 
+   if ($(targetElement).data("vote") === "great") {
+     voteCounts.great++;
+      }
+  else {
+    voteCounts.greatest++;
+  }
+     voteCounts.total++;
+       var percentGreat = (voteCounts.great/voteCounts.total)*100;
+  var greatest = (voteCounts.greatest/voteCounts.total)*100;
+        $(".great-progress").css("width", percentGreat+ "%");
+     $(".greatest-progress").css("width", greatest+ "%");
+   }); 
 
 });
